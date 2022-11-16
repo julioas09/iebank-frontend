5c60f05f23dd72c6dd3a5e62ce63313ccf7d6286
Julio Seijas <jus@microsoft.com>
2022-11-16 20:39:14 +0100

    refactor code for better readability and fix edit functionality

--- src/components/AppAccounts.vue
+++ src/components/AppAccounts.vue
@@ -1,34 +1,43 @@
 <template>
-    <div class="jumbotron vertical-center">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12">
-            <h1>Accounts</h1>
-            <hr />
-            <br />
-            <b-alert v-if=showMessage variant="success" show>{{ message }}</b-alert>
-            <button type="button" class="btn btn-success btn-sm" v-b-modal.account-modal>
-              Add Account
-            </button>
-            <br /><br />
-            <table class="table table-hover">
-              <thead>
-                <tr>
-                  <th scope="col">Account Name</th>
-                  <th scope="col">Account Number</th>
-                  <th scope="col">Account Balance</th>
-                  <th scope="col">Account Currency</th>
-                  <th scope="col">Account Status</th>
-                  <th scope="col">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="account in accounts" :key="account.id">
-                  <td>{{account.name}}</td>
-                  <td>{{account.number}}</td>
-                  <td>{{account.balance}}</td>
-                  <td>{{account.currency}}</td>
-                  <td>
+  <div class="jumbotron vertical-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h1>Accounts</h1>
+          <hr />
+          <br />
+          <!-- Allert Message -->
+          <b-alert v-if="showMessage" variant="success" show>{{
+            message
+          }}</b-alert>
+          <!-- b-alert v-if="error" variant="danger" show>{{ error }}</b-alert-->
+
+          <button
+            type="button"
+            class="btn btn-success btn-sm"
+            v-b-modal.account-modal
+          >
+            Create Account
+          </button>
+          <br /><br />
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Account Name</th>
+                <th scope="col">Account Number</th>
+                <th scope="col">Account Balance</th>
+                <th scope="col">Account Currency</th>
+                <th scope="col">Account Status</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="account in accounts" :key="account.id">
+                <td>{{ account.name }}</td>
+                <td>{{ account.account_number }}</td>
+                <td>{{ account.balance }}</td>
+                <td>{{ account.currency }}</td>
+                <td>
                   <span
                     v-if="account.status == 'Active'"
                     class="badge badge-success"
@@ -38,23 +47,34 @@
                     account.status
                   }}</span>
                 </td>
-                  <td>
-                    <div class="btn-group" role="group">
-                      <button type="button" class="btn btn-info btn-sm" v-b-modal.edit-account-modal @click="updateAccount(account)">
-                        Edit
-                      </button >
-                      <button type="button" class="btn btn-danger btn-sm" @click="deleteAccount(account)">
-                        Delete
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+                <td>
+                  <div class="btn-group" role="group">
+                    <button
+                      type="button"
+                      class="btn btn-info btn-sm"
+                      v-b-modal.edit-account-modal
+                      @click="editAccount(account)"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type="button"
+                      class="btn btn-danger btn-sm"
+                      @click="deleteAccount(account)"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <footer class="text-center">
+            Copyright &copy; All Rights Reserved.
+          </footer>
         </div>
-        <!-- Start of Modal for Create Account-->
-    <b-modal
+      </div>
+      <b-modal
         ref="addAccountModal"
         id="account-modal"
         title="Create a new account"
@@ -112,20 +132,19 @@
             <b-form-input
               id="form-edit-name-input"
               type="text"
-              v-model="createAccountForm.name"
+              v-model="editAccountForm.name"
               placeholder="Account Name"
               required
             >
             </b-form-input>
           </b-form-group>
-          <b-button type="submit" variant="outline-info">Submit</b-button>
+          <b-button type="submit" variant="outline-info">Update</b-button>
         </b-form>
       </b-modal>
       <!-- End of Modal for Edit Account-->
-
-      </div>
     </div>
-  </template>
+  </div>
+</template>
 
 <script>
 import axios from "axios";
@@ -143,12 +162,16 @@ export default {
         name: "",
       },
       showMessage: false,
-
+      message: "",
     };
   },
   methods: {
-    // GET accounts
-    getAccounts() {
+    /***************************************************
+     * RESTful requests
+     ***************************************************/
+
+    //GET function
+    RESTgetAccounts() {
       const path = `${process.env.VUE_APP_ROOT_URL}/accounts`;
       axios
         .get(path)
@@ -159,13 +182,14 @@ export default {
           console.error(error);
         });
     },
+
     // POST function
-    createAccount(payload) {
+    RESTcreateAccount(payload) {
       const path = `${process.env.VUE_APP_ROOT_URL}/accounts`;
       axios
         .post(path, payload)
         .then((response) => {
-          this.getAccounts();
+          this.RESTgetAccounts();
           // For message alert
           this.message = "Account Created succesfully!";
           // To actually show the message
@@ -174,20 +198,20 @@ export default {
           setTimeout(() => {
             this.showMessage = false;
           }, 3000);
-
         })
         .catch((error) => {
           console.error(error);
-          this.getAccounts();
+          this.RESTgetAccounts();
         });
     },
+
     // Update function
-    updateAccount(payload, accountId) {
+    RESTupdateAccount(payload, accountId) {
       const path = `${process.env.VUE_APP_ROOT_URL}/accounts/${accountId}`;
       axios
         .put(path, payload)
         .then((response) => {
-          this.getAccounts();
+          this.RESTgetAccounts();
           // For message alert
           this.message = "Account Updated succesfully!";
           // To actually show the message
@@ -199,16 +223,17 @@ export default {
         })
         .catch((error) => {
           console.error(error);
-          this.getAccounts();
+          this.RESTgetAccounts();
         });
     },
+
     // Delete account
     RESTdeleteAccount(accountId) {
       const path = `${process.env.VUE_APP_ROOT_URL}/accounts/${accountId}`;
       axios
         .delete(path)
         .then((response) => {
-          this.getAccounts();
+          this.RESTgetAccounts();
           // For message alert
           this.message = "Account Deleted succesfully!";
           // To actually show the message
@@ -220,20 +245,23 @@ export default {
         })
         .catch((error) => {
           console.error(error);
-          this.getAccounts();
+          this.RESTgetAccounts();
         });
     },
-    // Handle Delete button
-    deleteAccount(account) {
-      this.RESTdeleteAccount(account.id);
-    },
 
+    /***************************************************
+     * FORM MANAGEMENT
+     * *************************************************/
+
+    // Initialize forms empty
     initForm() {
       this.createAccountForm.name = "";
       this.createAccountForm.currency = "";
       this.editAccountForm.id = "";
       this.editAccountForm.name = "";
     },
+
+    // Handle submit event for create account
     onSubmit(e) {
       e.preventDefault(); //prevent default form submit form the browser
       this.$refs.addAccountModal.hide(); //hide the modal when submitted
@@ -241,9 +269,10 @@ export default {
         name: this.createAccountForm.name,
         currency: this.createAccountForm.currency,
       };
-      this.createAccount(payload);
-      this.initForm;
+      this.RESTcreateAccount(payload);
+      this.initForm();
     },
+
     // Handle submit event for edit account
     onSubmitUpdate(e) {
       e.preventDefault(); //prevent default form submit form the browser
@@ -251,18 +280,26 @@ export default {
       const payload = {
         name: this.editAccountForm.name,
       };
-      this.updateAccount(payload, this.editAccountForm.id);
+      this.RESTupdateAccount(payload, this.editAccountForm.id);
+      this.initForm();
     },
-    //Prepare edit account form with the name of the account
+
+    // Handle edit button
     editAccount(account) {
-      this.editAccountForm.name = account.name;
-      this.editAccountForm.id = account.id;
+      this.editAccountForm = account;
     },
 
-
+    // Handle Delete button
+    deleteAccount(account) {
+      this.RESTdeleteAccount(account.id);
+    },
   },
+
+  /***************************************************
+   * LIFECYClE HOOKS
+   ***************************************************/
   created() {
-    this.getAccounts();
+    this.RESTgetAccounts();
   },
 };
 </script>
